18d39c487728477fe54bbe60039530765bfeed82
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-11 12:55:06 +0200

    New Challenges:
    25. Opposite number
    26. Remove First and Last Character
    27. Return Negative
    28. String repeat
    29. Remove String Spaces
    30. Find the smallest integer in the array
    31. Is n divisible by x and y?
    32. A Needle in the Haystack

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-The challenges are taken from websites like codewars.com or edabit.com
-To look for a specific challenge just search in the file for the challenge name
+### The challenges are taken from websites like codewars.com or edabit.com.
+***
+**To look for a specific challenge just search in the JS file for 'Challenge Name'**  
+_For Example: to find the 12th challenge (Volume of a Box) just search for: Volume of a Box_
+***
+_The challenges are organized in a chronological order_
+
+### Challenge names:
+1. Reverse Array
+2. Power of number
+3. Maximum edge of triangle
+4. Return the Last Element in an Array
+5. Find the Perimeter of a Rectangle
+6. Say "Hello" Say "Bye"
+7. Frames Per Second
+8. Concatenating Two Integer Arrays
+9. Check if an Integer is Divisible By Five
+10. Is the Number Even or Odd?
+11. Compare Strings by Count of Characters
+12. Volume of a Box
+13. Find the Index
+14. Filter Repeating Character Strings
+15. Convert Number to Corresponding Month Name
+16. Reverse and Capitalize
+17. Add up the Numbers from a Single Number
+18. Matchstick Houses
+19. Find the Smallest and Biggest Numbers
+20. Check if One Array can be Nested in Another
+21. Find the Smallest and Biggest Numbers (part 2)
+22. altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+23. Is the string uppercase?
+24. Sum of positive
+25. Opposite number
+26. Remove First and Last Character
+27. Return Negative
+28. String repeat
+29. Remove String Spaces
+30. Find the smallest integer in the array
+31. Is n divisible by x and y?
+32. A Needle in the Haystack
--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-
-	// ESERCIZIO REVERSE ARRAY;
+//-----------------------------------------------------------------------------------------------------
+	// 1. Reverse Array
 // var array = [8,2,5,9,7,3,6,2]
 // var arrayReversed = reverseArrayInPlace(array);
 // console.log(arrayReversed);
 
 //	array.reverse(); 	// questo sotto e' come funziona reverse()
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO POTENZA DI UN NUMERO
+//-----------------------------------------------------------------------------------------------------
+	// 2. Power of number
 // var base = 5;
 // var exponent = 5;
 //
@@ -23,9 +24,10 @@
 //
 // 	return number;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Massima bordo di un triangolo
+//-----------------------------------------------------------------------------------------------------
+	// 3. Maximum edge of triangle
 // var side1 = 7;
 // var side2 = 9;
 //
@@ -36,9 +38,10 @@
 // 	var maximumEdge = (side1 + side2) - 1;
 // 	return maximumEdge;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Return the Last Element in an Array
+//-----------------------------------------------------------------------------------------------------
+	// 4. Return the Last Element in an Array
 		// Create a function that accepts an array and returns the last item in the array.
 
 // var arr = [1,2,3,4,5];
@@ -50,9 +53,10 @@
 // 	var numberLastItemArray = arr.length - 1;
 // 	return arr[numberLastItemArray];
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Find the Perimeter of a Rectangle
+//-----------------------------------------------------------------------------------------------------
+	// 5. Find the Perimeter of a Rectangle
 		// Create a function that takes height and width and finds the perimeter of a rectangle.
 
 // var height = 20;
@@ -65,9 +69,10 @@
 // 	var perimeter = (height + width) * 2;
 // 	return perimeter;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Say "Hello" Say "Bye"
+//-----------------------------------------------------------------------------------------------------
+	// 6. Say "Hello" Say "Bye"
 		// Write a function that takes a string name and a number num (either 0 or 1)
 		// and return "Hello" + name if num is 1, otherwise return "Bye" + name.
 		// The name you return must be capitalized.
@@ -87,9 +92,10 @@
 // 		return "Bye " + stringCapitalized;
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Frames Per Second
+//-----------------------------------------------------------------------------------------------------
+	// 7. Frames Per Second
 	/* Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
 	 	NOTES: FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
 			   Assume the screen produces 60 frames every second. */
@@ -105,9 +111,10 @@
 // 	var fps = minutes * fps * 60
 // 	return fps;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Concatenating Two Integer Arrays
+//-----------------------------------------------------------------------------------------------------
+	// 8. Concatenating Two Integer Arrays
 	/* Create a function to concatenate two integer arrays */
 
 // var arr1 = [1, 3, 5];
@@ -125,9 +132,10 @@
 //
 // 	return array1and2;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Check if an Integer is Divisible By Five
+//-----------------------------------------------------------------------------------------------------
+	// 9. Check if an Integer is Divisible By Five
 	/* Create a function that returns true if an integer is evenly divisible by 5, and false otherwise. */
 
 // var n = 7;
@@ -143,9 +151,10 @@
 // 		return false;
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Is the Number Even or Odd?
+//-----------------------------------------------------------------------------------------------------
+	// 10. Is the Number Even or Odd?
 	/* Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers. */
 
 // var num = 12;
@@ -161,9 +170,10 @@
 // 		return 'odd';
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Compare Strings by Count of Characters
+//-----------------------------------------------------------------------------------------------------
+	// 11. Compare Strings by Count of Characters
 	/*  Create a function that takes two strings as arguments and return either
 		true or false depending on whether the total number of characters in the first
 		string is equal to the total number of characters in the second string. */
@@ -182,9 +192,10 @@
 // 		return false;
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Volume of a Box
+//-----------------------------------------------------------------------------------------------------
+	// 12. Volume of a Box
 	/*  Create a function that takes an object argument sizes (contains width, length, height keys)
 		and returns the volume of the box.
 		NOTES: volume is length * width * height
@@ -205,9 +216,10 @@
 //
 // 	return volume;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	// ESERCIZIO Find the Index
+//-----------------------------------------------------------------------------------------------------
+	// 13. Find the Index
 	/*  Create a function that takes an array and a string as arguments and return the index of the string.*/
 
 // var arr = ['abc', 'ghj', 'banana', 'grape'];
@@ -225,9 +237,11 @@
 // 	}
 // 	return strIndex
 // }
+//-----------------------------------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------------------------------
 	// --------------------------------- DA RIGUARDARE ---------------------------------
-	// ESERCIZIO Filter Repeating Character Strings
+	// 14. Filter Repeating Character Strings
 	/*  Create a function that keeps only strings with repeating identical characters (in other words, it has a set size of 1).
 		NOTES: 	A string with a single character is trivially counted as a string with repeating identical characters.
 				If there are no strings with repeating identical characters, return an empty array (see example #3).*/
@@ -258,9 +272,10 @@
 // 	}
 // 	return arraySingleChar;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	/* ESERCIZIO Convert Number to Corresponding Month Name
+//-----------------------------------------------------------------------------------------------------
+	/* 15. Convert Number to Corresponding Month Name
 		>Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string.
 		For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
 		NOTES: 	You can expect only integers ranging from 1 to 12 as test input. */
@@ -275,9 +290,10 @@
 // 	'July', 'August', 'September', 'October', 'November', 'December'];
 // 	return arrayMonth[num-1];
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	/* ESERCIZIO Reverse and Capitalize
+//-----------------------------------------------------------------------------------------------------
+	/* 16. Reverse and Capitalize
 		>Create a function that takes a string of lowercase characters and returns that string reversed and in upper case.*/
 
 // var str = 'CIAO';
@@ -295,9 +311,10 @@
 // 	var newStringCapitalized = newStringReversed.toUpperCase();
 // 	return newStringCapitalized;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	/* ESERCIZIO Add up the Numbers from a Single Number
+//-----------------------------------------------------------------------------------------------------
+	/* 17. Add up the Numbers from a Single Number
 		>Create a function that takes a number as an argument.
 		Add up all the numbers from 1 to the number you passed to the function.
 		For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
@@ -316,9 +333,10 @@
 // 	}
 // 	return sum;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	/* ESERCIZIO Matchstick Houses
+//-----------------------------------------------------------------------------------------------------
+	/* 18. Matchstick Houses
 		>Create a function that takes a number (step) as an argument and returns
 		the amount of matchsticks in that step. See step 1, 2 and 3 in the image below.
 			https://edabit-challenges.s3.amazonaws.com/matchstick_houses.png
@@ -338,9 +356,10 @@
 // 		return (5 * step) + 1;
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-	/* ESERCIZIO Find the Smallest and Biggest Numbers
+//-----------------------------------------------------------------------------------------------------
+	/* 19. Find the Smallest and Biggest Numbers
 		>Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
 		NOTES: Note the strict inequality
 	*/
@@ -368,9 +387,11 @@
 //
 // 	return arrayMinMax;
 // }
+//-----------------------------------------------------------------------------------------------------
 
 
-	/* ESERCIZIO Check if One Array can be Nested in Another
+//-----------------------------------------------------------------------------------------------------
+	/* 20. Check if One Array can be Nested in Another
 		>Create a function that returns true if the first array can be nested inside the second.
 		arr1 can be nested inside arr2 if:
 			arr1's min is greater than arr2's min.
@@ -412,9 +433,10 @@
 // 		minNum: minNum
 // 	}
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-    /* ESERCIZIO Find the Smallest and Biggest Numbers
+//-----------------------------------------------------------------------------------------------------
+    /* 21. Find the Smallest and Biggest Numbers (part 2)
         >Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
         NOTES: Note the strict inequality
     */
@@ -432,9 +454,10 @@
 //         return false;
 //     }
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-    /* ESERCIZIO altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+//-----------------------------------------------------------------------------------------------------
+    /* 22. altERnaTIng cAsE <=> ALTerNAtiNG CaSe
         >each lowercase letter becomes uppercase and each uppercase letter becomes lowercase
         NOTES:
     */
@@ -457,9 +480,10 @@
 //
 //     return stringSwitch;
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-    /* ESERCIZIO Is the string uppercase?
+//-----------------------------------------------------------------------------------------------------
+    /* 23. Is the string uppercase?
         >a string is said to be in ALL CAPS whenever it does not contain any
          lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
         NOTES:
@@ -477,9 +501,10 @@
 //         return false;
 //     }
 // }
+//-----------------------------------------------------------------------------------------------------
 
-
-    /* ESERCIZIO Sum of positive
+//-----------------------------------------------------------------------------------------------------
+    /* 24. Sum of positive
         >You get an array of numbers, return the sum of all of the positives ones.
         NOTES:
     */
@@ -500,7 +525,182 @@
 //
 //     return sum;
 // }
+//-----------------------------------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------------------------------
+    /* 25. Opposite number
+        >Very simple, given a number, find its opposite.
+        NOTES:
+    */
+
+// var number = 5;
+//
+// var funzione = opposite(number);
+// console.log(funzione);
+//
+// function opposite(number) {
+//     return number * -1;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 26. Remove First and Last Character
+        >It's pretty straightforward. Your goal is to create a function that
+         removes the first and last characters of a string. You're given one parameter,
+         the original string. You don't have to worry with strings with less than two characters.
+        NOTES:
+    */
+
+// var str = 'bella';
+//
+// var funzione = removeChar(str);
+// console.log(funzione);
+//
+// function removeChar(str) {
+//     return str.substring(1, (str.length - 1));
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 27. Return Negative
+        >In this simple assignment you are given a number and have to make it negative.
+         But maybe the number is already negative?
+        NOTES:
+    */
+
+// var num = -40;
+//
+// var funzione = makeNegative(num);
+// console.log(funzione);
+//
+// function makeNegative(num) {
+//     if (num > 0) {
+//         return num * -1;
+//     } else {
+//         return num;
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 28. String repeat
+        >Write a function called repeatString which repeats the given String src
+         exactly count times.
+        NOTES:
+    */
+
+// var n = 5;
+// var s = 'Hello';
+//
+// var funzione = repeatStr(n, s);
+// console.log(funzione);
+//
+// function repeatStr(n, s) {
+//     var repeatedString = '';
+//
+//     for (var i = 1; i <= n; i++) {
+//         repeatedString += s;
+//     }
+//
+//     return repeatedString;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 29. Remove String Spaces
+        >Simple, remove the spaces from the string, then return the resultant string.
+        NOTES:
+    */
+
+// var x = 'Hello HolaCiao';
+//
+// var funzione = noSpace(x);
+// console.log(funzione);
+//
+// function noSpace(x) {
+//     var noSpaceStr = '';
+//
+//     for (var i = 0; i < x.length; i++) {
+//         if (x[i] != ' ') {
+//             noSpaceStr += x[i];
+//         }
+//     }
+//
+//     return noSpaceStr;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 30. Find the smallest integer in the array
+        >Given an array of integers your solution should find the smallest integer.
+        NOTES:
+    */
+
+// var arr = [34, -345, -1, 100];
+//
+// var funzione = minMax(arr);
+// console.log(funzione);
+//
+// function minMax(arr) {
+// 	var minNum = arr[0];
+//
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (arr[i] < minNum) {
+// 			minNum = arr[i];
+// 		}
+// 	}
+//
+// 	return minNum;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 31. Is n divisible by x and y?
+        >Create a function that checks if a number n is divisible by two numbers x AND y.
+         All inputs are positive, non-zero digits.
+        NOTES:
+    */
+
+// var n = 3;
+// var x = 3;
+// var y = 4;
+//
+// var funzione = isDivisible(n, x, y);
+// console.log(funzione);
+//
+// function isDivisible(n, x, y) {
+//     if ((n % x == 0) && (n % y == 0)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 32. A Needle in the Haystack
+        >Can you find the needle in the haystack?
+            Write a function findNeedle() that takes an array full of junk but containing one "needle"
+            After your function finds the needle it should return a message (as a string) that says:
+            "found the needle at position " plus the index it found the needle, so:
+            findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+            should return "found the needle at position 5"
+        NOTES:
+    */
+
+// var haystack = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
+//
+// var funzione = findNeedle(haystack);
+// console.log(funzione);
+//
+// function findNeedle(haystack) {
+//     for (var i = 0; i < haystack.length; i++) {
+//         if (haystack[i] == 'needle') {
+//             return "found the needle at position " + i;
+//         }
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
 
 
 
