f11931c195d5c83966e1dd5d66142cbe9c96a134
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-13 10:56:12 +0200

    Two ways of solving (46.) recursive and iterative

--- main.js
+++ main.js
@@ -1178,19 +1178,23 @@
 //-----------------------------------------------------------------------------------------------------
         // 46. Practice creating a binary search algorithm
 
-// var numbers = [96, 58, 24, 30, 18, 32, 90, 63, 87, 56, 8, 15, 86, 74, 80, 99, 60,
-//                49, 97, 25, 77, 67, 57, 11, 95, 70, 7, 59, 19, 3, 23, 93, 66, 45,
-//                84, 21, 48, 83, 6, 16, 9];
-//
-// var arrClone = numbers;
-// var arrSorted = arrClone.sort(function(a, b){return a-b});
-// console.log(arrSorted);
-//
-//
-// var posizioneX = binarySearch(arrSorted, 0, arrSorted.length - 1, 63);
-// console.log(posizioneX);
-//
-// function binarySearch(arr, l, r, x) {    // return the index of x
+var numbers = [96, 58, 24, 30, 18, 32, 90, 63, 87, 56, 8, 15, 86, 74, 80, 99, 60,
+               49, 97, 25, 77, 67, 57, 11, 95, 70, 7, 59, 19, 3, 23, 93, 66, 45,
+               84, 21, 48, 83, 6, 16, 9];
+
+var arrClone = numbers;
+var arrSorted = arrClone.sort(function(a, b){return a-b});
+console.log(arrSorted);
+
+var t0 = performance.now();
+
+var posizioneX = binarySearch(arrSorted, 0, arrSorted.length - 1, 99);
+console.log(posizioneX);
+
+var t1 = performance.now();
+console.log('It took ' + (t1 - t0) + ' milliseconds');
+
+// function binarySearch(arr, l, r, x) {    // return the index of x (recursive)
 //     if (r >= l) {
 //         var mid = Math.floor(l + (r -l) / 2);
 //         if (arr[mid] == x) {
@@ -1203,6 +1207,19 @@
 //     } else {
 //         return -1;
 //     }
+// }
+// function binarySearch(arr, l, r, x) {    // return the index of x (iterative)
+//     while (l <= r) {
+//         var mid = Math.floor(l + (r -l) / 2);
+//         if (arr[mid] == x) {
+//             return mid;
+//         } else if (arr[mid] > x) {
+//             r = mid - 1;
+//         } else {
+//             l = mid + 1;
+//         }
+//     }
+//     return -1;
 // }
 //-----------------------------------------------------------------------------------------------------
 
